1246f2f9979d5d5c8cc9b6e6a8caecd621752a08
Business Compliance <noksmazzi@gmail.com>
2025-12-04 10:27:47 +0200

    Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@
 const BACKEND_MODE = "supabase";
 
 // Supabase
-const SUPABASE_URL = "https://YOUR_SUPABASE_URL.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://rqhwelbfjnhzsihjpiub.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxaHdlbGJmam5oenNpaGpwaXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTI0MzIsImV4cCI6MjA4MDM4ODQzMn0.T5ui1VXuAGV7sLOXw3Phfy3LS8fbDujFgLBR-b6pI80";
 
 // Google Sheets (optional)
 const GOOGLE_SHEET_WEB_APP_URL = "";
